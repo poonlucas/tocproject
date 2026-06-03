c6d7811b8f58ebcf839d437cfa77b416cf5ea658
BrandynTucknott <brandyntucknott2021@gmail.com>
2026-06-02 23:41:37 -0700

    update code to reflect awareness of bugs in DeltaMaxSAT._solve().

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-# TUTORIAL SCRIPT FROM WEBSITE TO GET A FEEL FOR HOW TO USE PYSAT
-# (-x1 OR x2) AND (-x1 OR -x2)
-#cnf = CNF(from_clauses=[[-1, 2], [-1,-2]])
-
-#with Solver(bootstrap_with=cnf) as solver:
-    # call solver for this formula
- #   print('formula is', f'{"s" if solver.solve() else "uns"}atisfiable')
-
-    # the formula is satisfiable, thus has a model
-  #  print('and the model is:', solver.get_model())
-
-    # minisat assumption inferface?
-   # print('formula is', f'{"s" if solver.solve(assumptions=[1, 2]) else "uns"}atisfiable', 'assuming x1 and x2')
-
-    # the formula is unsatisfiable
-    #print('and the unsatisfiable core is:', solver.get_core())
-
 # class to solve a delta-3-MAX-SAT
 #   cnf: CNF solver from PySAT
 #   phi: boolean formula
@@ -65,16 +48,18 @@ class DeltaMaxSAT:
 
         cnf = CNF(from_clauses=clauses)
         with Solver(bootstrap_with=cnf) as solver:
+            # TODO: THIS SECTION HAS BIG BUGS!!!
+            # RECURSION NOT CORRECTLY IMPLEMENTED
             status = solver.solve()
             permutations = [clauses[:i] + clauses[i+1:] for i in range(len(clauses))] # all possible combinations with one element removed
             if not status: # current num clauses invalid, try one less
                 for _cnf in permutations:
-                    return _solve(max_num_clauses - 1, _cnf)
+                    return self._solve(max_num_clauses - 1, _cnf)
 
             inBound = float(max_num_clauses) / float(len(self.phi)) >= 1.0 - self.delta
             if not inBound: # current num clauses invalid, try one less
                 for _cnf in permutations:
-                    return _solve(max_num_clauses - 1, _cnf)
+                    return self._solve(max_num_clauses - 1, _cnf)
 
             # CNF is solvable and in-bound
             return (max_num_clauses, clauses, solver.get_model())
