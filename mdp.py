129498b56762668b98dc7236ab6decd42d010bc4
Lucas Poon <lucaslfpoon@gmail.com>
2026-06-10 19:10:53 -0700

    preference mdp

--- mdp.py
+++ mdp.py
@@ -5,46 +5,49 @@ import numpy as np
 import graphviz
 
 
-def check_clause(clause, assignment):
-    for literal in clause:
-        if (literal > 0 and assignment[abs(literal) - 1] == 1) or (literal < 0 and assignment[abs(literal) - 1] == 0):
-            return True
-    return False
+def check_preference(preference, assignment):
+    for action in preference:
+        if (action > 0 and assignment[abs(action) - 1] == 1) or (action < 0 and assignment[abs(action) - 1] == 0):
+            return False
+    return True
 
 
 class MDP:
-    def __init__(self, phi, num_variables: int, num_clauses: int, solution, delta):
-        self.phi = phi
-        self.horizon = num_variables + 1
-        self.num_clauses = num_clauses
-        self.solution = [-1] * num_variables
-        for v in solution:
-            self.solution[abs(v) - 1] = 1 if v > 0 else 0
-        self.delta = delta
+    def __init__(self, horizon: int = 5, max_preferences: int = 10, seed: int = 0):
+        np.random.seed(seed)
+        self.max_preferences = max_preferences
+        self.horizon = horizon
 
         self.states = []
         for h in range(1, self.horizon + 1):
             assigned = np.array(list(product([0, 1], repeat=h-1)), dtype=int)
-            unassigned = -np.ones((2 ** (h-1), num_variables - (h-1)), dtype=int)
+            unassigned = -np.ones((2 ** (h-1), self.horizon - 1 - (h-1)), dtype=int)
             states_h = np.concatenate((assigned, unassigned), axis=1)
             self.states.append(states_h)
 
-        max_reward = max([self.reward(state) for state in self.states[-1]])
-        sol_reward = self.reward(self.solution)
-        print(f"Gap: {max_reward - sol_reward}, epsilon-optimal: {max_reward - sol_reward <= (self.delta / 2)}")
+        self.preferences = self.createPreferences()
+
+    def createPreferences(self):
+        preferences = [[]] * self.max_preferences
+        var_choices = [x for x in range(-(self.horizon - 1), self.horizon)]
+        var_choices.pop(self.horizon - 1)
+        for i in range(self.horizon - 1):
+            R = np.random.randint(1, self.horizon)
+            preferences[i] = np.random.choice(var_choices, size=R, replace=False)
+        return preferences
 
     def reward(self, assignment):
-        correct_clauses = [check_clause(clause, assignment) for clause in self.phi]
-        return correct_clauses.count(True) / self.num_clauses
+        violated_preferences = [check_preference(preference, assignment) for preference in self.preferences]
+        return 1 - violated_preferences.count(True) / len(self.preferences)
 
-    def visualize(self):
+    def visualize(self, solution):
         graph = graphviz.Digraph(comment='MDP')
         graph.attr(rankdir='LR')
         for h, state_h in enumerate(self.states):
             with graph.subgraph(name=str(h)) as c:
                 for state in state_h:
                     color = 'black'
-                    if np.all(state[:h] == self.solution[:h]):
+                    if np.all(state[:h] == solution[:h]):
                         color = 'red'
                     c.node(str(state),
                            label=(str(state) if h < self.horizon - 1 else f'{state}, {self.reward(state)}'),
@@ -55,8 +58,3 @@ class MDP:
                         c.edge(str(prev), str(state), color=color)
 
         graph.render(directory='mdp', view=True)
-
-
-if __name__ == '__main__':
-    mdp = MDP([[-1, 2, -3], [1, 2, 4]], 4, 2, [-1, 2, 3, 4], 0.8)
-    mdp.visualize()
